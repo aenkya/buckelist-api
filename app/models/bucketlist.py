6d2058bdbfca8de3d3883d7907d433d3696b1839
Bruce Bigirwenkya <brucebigirwenkya@gmail.com>
2017-08-10 06:30:29 +0300

    Inherit From BaseModel Reduce on repetition of code with inheritence of common functions and attributes

---
+++ app/models/bucketlist.py
@@ -0,0 +1,24 @@
+from app.models.baseModel import BaseModel
+from app import db
+
+
+class Bucketlist(BaseModel):
+    '''This class represents the bucketlist model'''
+
+    __table__name = 'bucketlist'
+
+    name = db.Column(db.String(255), nullable=False)
+    items = db.relationship(
+        'Item', cascade='all, delete', backref='bucketlist')
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    @staticmethod
+    def get_all():
+        '''get all bucketlist items'''
+        return Bucketlist.query.all()
+
+    def __repr__(self):
+        return "<Bucketlist: {}>".format(self.name)
+
+    def __str__(self):
+        return '{0}'.format(self.name)
--- app/models/item.py
+++ app/models/item.py
@@ -1,34 +1,16 @@
+from app.models.baseModel import BaseModel
 from app import db
 
 
-class Item(db.Model):
+class Item(BaseModel):
     '''This class represents the item model'''
 
     __table__name = 'item'
 
-    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text, nullable=False)
     done = db.Column(db.Boolean, default=False)
-    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
-    date_modified = db.Column(
-        db.DateTime, default=db.func.current_timestamp(),
-        onupdate=db.func.current_timestamp())
     bucketlist_id = db.Column(db.Integer,
                               db.ForeignKey('bucketlist.id'), nullable=False)
-
-    def __init__(self, name):
-        '''initialize with name'''
-        self.name = name
-
-    def save(self):
-        '''save data to database'''
-        db.session.add(self)
-        db.session.commit()
-
-    def delete(self):
-        '''delete data from database'''
-        db.session.delete(self)
-        db.session.commit()
 
     def __repr__(self):
         return "<Item: {}>".format(self.name)
